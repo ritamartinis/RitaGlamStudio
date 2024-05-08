1cdba9d3c35c5107d53b249a307f9f1ed65190b4
ritamartinis <r_ita16@hotmail.com>
2024-05-08 17:27:08 +0100

    Criação - 08.05

---
+++ RitaGlamStudio.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RitaGlamStudio.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public required string Name { get; set; }
+    }
+}
